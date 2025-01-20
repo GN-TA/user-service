d2d573523b1a758209a3a37a94b9ee01353ca478
codeTheStudent <hjun303@gnu.ac.kr>
2025-01-20 20:31:41 +0900

    fix : tenant user entity 수정

--- src/main/java/site/iotify/userservice/domain/tenant/entity/TenantUser.java
+++ src/main/java/site/iotify/userservice/domain/tenant/entity/TenantUser.java
@@ -7,28 +7,41 @@ import site.iotify.userservice.entity.User;
 
 import java.io.Serializable;
 
+import lombok.*;
+
 @Entity
 @Table(name = "tenant_user")
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TenantUser {
+
     @EmbeddedId
     private PK pk;
 
-    @JoinColumn(name = "user_id")
     @ManyToOne
+    @MapsId("userId") // PK의 userId와 매핑
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private User user;
 
-    @JoinColumn(name = "tenant_id")
     @ManyToOne
+    @MapsId("tenantId") // PK의 tenantId와 매핑
+    @JoinColumn(name = "tenant_id", insertable = false, updatable = false)
     private Tenant tenant;
 
     @Embeddable
-    @AllArgsConstructor
+    @Getter
+    @Setter
     @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
     public static class PK implements Serializable {
         @Column(name = "user_id")
         private String userId;
+
         @Column(name = "tenant_id")
         private String tenantId;
     }
 }
+
