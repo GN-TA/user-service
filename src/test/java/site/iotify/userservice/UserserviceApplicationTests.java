0e9bf29823174b2b9f8f7286933c8a6d8dea323d
codeTheStudent <hjun303@gnu.ac.kr>
2025-04-14 14:56:05 +0900

    fix : test 시 외부 의존성 제거

--- src/test/java/site/iotify/userservice/UserserviceApplicationTests.java
+++ src/test/java/site/iotify/userservice/UserserviceApplicationTests.java
@@ -2,11 +2,18 @@ package site.iotify.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import site.iotify.userservice.domain.user.service.MinioService;
+import site.iotify.userservice.global.adaptor.ChirpstackAdaptor;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class UserserviceApplicationTests {
+    @MockBean
+    MinioService minioService;
+    @MockBean
+    ChirpstackAdaptor chirpstackAdaptor;
 
     @Test
     void contextLoads() {
--- src/test/java/site/iotify/userservice/adaptor/ChirpstackAdaptorTest.java
+++ src/test/java/site/iotify/userservice/adaptor/ChirpstackAdaptorTest.java
@@ -3,19 +3,25 @@ package site.iotify.userservice.adaptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import site.iotify.userservice.domain.user.service.MinioService;
 import site.iotify.userservice.global.adaptor.ChirpstackAdaptor;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class ChirpstackAdaptorTest {
+    @MockBean
+    MinioService minioService;
+    @MockBean
+    ChirpstackAdaptor chirpstackAdaptor; // or mock whatever its dependencies are
 
     @Autowired
-    private ChirpstackAdaptor chirpstackAdaptor;
+    private ChirpstackAdaptor adaptor;
 
     @Test
     void getUser() {
         String id = "e4cb2550-d93c-4627-968d-fdfd113da28d";
-        System.out.println(chirpstackAdaptor.getUser(id));
+        System.out.println(adaptor.getUser(id));
     }
 }
