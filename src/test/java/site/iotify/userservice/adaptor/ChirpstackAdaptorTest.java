d39ab0d1849344a05cc4f5798e324011d04ec654
codeTheStudent <hjun303@gnu.ac.kr>
2025-04-14 14:49:06 +0900

    fix : test 시 외부 의존성 제거

--- src/test/java/site/iotify/userservice/adaptor/ChirpstackAdaptorTest.java
+++ src/test/java/site/iotify/userservice/adaptor/ChirpstackAdaptorTest.java
@@ -3,8 +3,10 @@ package site.iotify.userservice.adaptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import site.iotify.userservice.global.adaptor.ChirpstackAdaptor;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ChirpstackAdaptorTest {
 
